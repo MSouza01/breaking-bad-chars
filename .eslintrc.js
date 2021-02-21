2e97761f37768d4580c48641a6f0940c5b2de2ac
Moacyr Souza <mtfsouza.ms@gmail.com>
2021-02-21 16:12:20 -0300

    Initial project setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,43 @@
 module.exports = {
-	env: {
-		es2021: true,
-		node: true,
-	},
-	extends: ['eslint:recommended', 'prettier'],
-	plugins: ['prettier'],
-	parserOptions: {
-		ecmaVersion: 12,
-		sourceType: 'module',
-	},
-	rules: {
-		indent: ['error', 'tab'],
-		'linebreak-style': ['error', 'windows'],
-		quotes: ['error', 'single'],
-		semi: ['error', 'always'],
-		'class-methods-use-this': 'off',
-		'no-param-reassign': 'off',
-		camelcase: 'off',
-	},
+  env: {
+    es6: true,
+    node: true,
+    browser: true,
+    jest: true,
+  },
+  extends: ['eslint:recommended', 'prettier'],
+  plugins: ['react'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 6,
+    ecmaFeatures: {
+      jsx: true,
+    },
+    sourceType: 'module',
+  },
+  rules: {
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+    indent: [
+      'error',
+      2,
+      {
+        SwitchCase: 1,
+      },
+    ],
+    'linebreak-style': ['error', process.platform === 'win32' ? 'windows' : 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'class-methods-use-this': 'off',
+    'no-param-reassign': 'off',
+    'no-unused-vars': ['warn'],
+  },
+  overrides: [
+    {
+      files: ['**/*.spec.js', '**/*.spec.jsx'],
+      env: {
+        jest: true,
+      },
+    },
+  ],
 };
